17384ffbf0d3e4cbd1197c6d04f059ab42c6fa30
윤영우 <macair1995@naver.com>
2024-01-04 23:18:58 +0900

    Update application.properties

---
+++ src/main/java/com/thehecklers/planefinder/PositionReporter.java
@@ -0,0 +1,29 @@
+package com.thehecklers.planefinder;
+
+import com.thehecklers.planefinder.domain.Aircraft;
+import com.thehecklers.planefinder.service.PlaneFinderService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.function.Supplier;
+
+@AllArgsConstructor
+@Configuration
+public class PositionReporter {
+    private final PlaneFinderService pfService;
+
+    @Bean
+    Supplier<Iterable<Aircraft>> reportPositions() {
+        return () -> {
+          try {
+              return pfService.getAircraft();
+          } catch (IOException e) {
+              e.printStackTrace();
+          }
+          return List.of();
+        };
+    }
+}
